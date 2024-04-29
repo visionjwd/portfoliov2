3eb3e5a9802e5b3779ad38e52b2e7cdb689200c7
Junwoo <jwoh@uwaterloo.ca>
2024-04-28 22:27:19 -0400

    hexa update

--- pages/hexapod.js
+++ pages/hexapod.js
@@ -32,7 +32,30 @@ function hexapod() {
         <div className = {styles.overview}>
             <div className = {styles.flex2}>
                 <h2>MECHANICAL DESIGN</h2>
-                <p>- </p>
+                <p><span>Initial Calculations: </span>Each MG996R servos have a torque rating of 12kg/cm, so keeping that in mind the body was designed to be minimal in weight. Printed at just 10% infill in order to keep it lightweight while keeping the design asthetic, 2 lightweight carbon fiber rods were utilized as structural support for the main body.</p>
+                <p><span>Topology Optimization: </span> Forthe legs, topology optimization feature in combination with FEA analysis in Fusion360 was used to minimize weight, with each component&#39;s optimization setting at 60% of original weight.</p>
+                <p>As well as these, proper tolerance values for a transitional fit was utilized. This stage also involved measuring my 3D printer&#39;s dimensional accuracy values, therefore each attachment&#39;s tolerance values were adjusted .25mm in the x direction and -0.1mm in the y direction accordingly.</p>
+            </div>
+            <div className = {styles.flex1}>
+
+            </div>
+        </div>
+        <div className = {styles.overview}>
+            <div className = {styles.flex1}>
+
+            </div>
+            <div className = {styles.flex2}>
+                <h2>HARDWARE CONFIGURATION</h2>
+                <p>Each Servo used has a voltage rating of 6v, with max current at stall being 2.4A. In order to supply a constant voltage for all 18 servos, a 3S lipo battery was utilized with a DC buck converter to step the voltage down from 11.6v to 5.5V, as well as setting a max current rating of 5A to ensure that the PCA9685 servo drivers are protected.</p>
+                <p>ESP32 was used for it's built in bluetooth connectivity which I am using to pair it to my PS4 controller for inputs. With the 3S battery's voltage being 11.6V, I can wire it directly to the ESP32 without needing to step down the voltage again.</p>
+                <p>All the hardware components are mounted directly in the center of the bot as to keep the balance.</p>
+            </div>
+        </div>
+        <div className = {styles.overview}>
+            <div className = {styles.flex2}>
+                <h2>INVERSE KINEMATICS</h2>
+                <p>In regards to the Inverse Kinematics algorithm, each leg utilizes the same formula within it's local coordinates, which are then unified with global coordinates with repect to the Hexapod&#39;s center.</p>
+                <p>Utilizing PlatformIO over Arduino IDE for firmware development was a superior choice as it made it easier to follow a more traditional C++ programming environment, as well as being able to utilize trusted libraries for PS4 controller input and output.</p>
             </div>
             <div className = {styles.flex1}>
 
